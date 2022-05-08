ba913ed0b9c848244912093719c74243bdc97d56
Laurent Demailly <ldemailly@gmail.com>
2022-05-08 04:25:59 +0000

    switch to default port for grpc (8079)

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -x
-nohup fortio server -http-port disabled -tcp-port disabled -udp-port disabled -redirect-port disabled -grpc-port 8443 &
+nohup fortio server -http-port disabled -tcp-port disabled -udp-port disabled -redirect-port disabled -grpc-port 8079 &
 cd fortio_data
 nohup fortio report -http-port 80 -redirect-port disabled &
