a7c9b804c4e1659f114fb91469cd85eb60ef5bd4
Laurent Demailly <ldemailly@gmail.com>
2022-05-21 22:20:51 -0700

    don't overwrite old logs

--- startup.sh
+++ startup.sh
@@ -4,11 +4,11 @@ set -x
 pkill fortio
 pkill proxy
 nohup fortio server -http-port disabled -tcp-port disabled -udp-port disabled\
-   -redirect-port disabled -grpc-port 8079 -P "8443 127.0.0.1:443" > grpc.log &
+   -redirect-port disabled -grpc-port 8079 -P "8443 127.0.0.1:443" >> grpc.log &
 cd fortio_data
-nohup fortio report -http-port 8080 -redirect-port disabled > ../report.log &
+nohup fortio report -http-port 8080 -redirect-port disabled >> ../report.log &
 cd ../fortio_https
-nohup proxy -email fortio@fortio.org -h2 -config config/ > ../proxy.log &
+nohup proxy -email fortio@fortio.org -h2 -config config/ >> ../proxy.log &
 cd ..
 sleep 3
 tail -f proxy.log
