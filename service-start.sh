c7a3b9d4b85efbfa6f6301cfd53965f69a82a22e
Laurent Demailly <ldemailly@gmail.com>
2022-06-09 05:12:29 -0700

    keep logs were we want

--- service-start.sh
+++ service-start.sh
@@ -3,9 +3,9 @@ set -x
 pkill fortio
 pkill proxy
 PATH=$HOME/go/bin:$PATH
-nohup fortio server -http-port disabled -tcp-port disabled -udp-port disabled\
-   -redirect-port disabled -grpc-port 8079 -P "8443 127.0.0.1:443" >> grpc.log &
+fortio server -http-port disabled -tcp-port disabled -udp-port disabled\
+   -redirect-port disabled -grpc-port 8079 -P "8443 127.0.0.1:443" >> grpc.log 2>&1 &
 cd fortio_data
-nohup fortio report -http-port 8080 -redirect-port disabled >> ../report.log &
+fortio report -http-port 8080 -redirect-port disabled >> ../report.log 2>&1 &
 cd ../fortio_https
-nohup proxy -email fortio@fortio.org -h2 -config config/ >> ../proxy.log &
+proxy -email fortio@fortio.org -h2 -config config/ >> ../proxy.log 2>&1 &
