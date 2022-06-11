f268f2b071401e60260a4f56991515d83609b062
Laurent Demailly <ldemailly@gmail.com>
2022-06-10 19:27:10 -0700

    local install and restart

--- update.sh
+++ update.sh
@@ -1,14 +1,7 @@
 #! /bin/sh
 set -x
+CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/fortio@latest
+CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/proxy@latest
 ./bind_to_443.sh `which proxy`
-pkill fortio
-pkill proxy
-nohup fortio server -http-port disabled -tcp-port disabled -udp-port disabled\
-   -redirect-port disabled -grpc-port 8079 -P "8443 127.0.0.1:443" >> grpc.log &
-cd fortio_data
-nohup fortio report -http-port 8080 -redirect-port disabled >> ../report.log &
-cd ../fortio_https
-nohup proxy -email fortio@fortio.org -h2 -config config/ >> ../proxy.log &
-cd ..
-sleep 3
+sudo systemctl restart fortio
 tail -f proxy.log
