12944a68523533e828b2baa8dbe96e5de313d7ec
Laurent Demailly <ldemailly@gmail.com>
2023-02-20 12:26:57 -0800

    add go version output

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 set -x
+go version
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/fortio@latest
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/proxy@latest
 ./bind_to_443.sh `which proxy`
