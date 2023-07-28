6ccdaf27df1dafd577d5747e4a7f0035ac3cec98
Laurent Demailly <ldemailly@gmail.com>
2023-07-28 09:45:59 -0700

    add logc

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ set -x
 go version
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/fortio@latest
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/proxy@latest
+CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/logc@latest
 ./bind_to_443.sh `which proxy`
 sudo systemctl restart fortio
-tail -f proxy.log
+tail -f proxy.log | logc
