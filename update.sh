0a195ea9240bf5f9ad9591917278610d66bcd9d6
Laurent Demailly <ldemailly@gmail.com>
2023-08-03 14:35:13 -0700

    force color mode so it works from ssh remote command

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 set -x
 go version
+git pull
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/fortio@latest
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/proxy@latest
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/logc@latest
 ./bind_to_443.sh `which proxy`
 sudo systemctl restart fortio
-tail -f proxy.log | logc
+tail -f proxy.log | logc -logger-force-color
