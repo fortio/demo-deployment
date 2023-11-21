4f4f7e5c5a618612fba5956919f6dedf0770c4ba
Laurent Demailly <ldemailly@gmail.com>
2023-11-21 14:42:25 -0800

    debug bad index.tsv

--- update.sh
+++ update.sh
@@ -7,4 +7,5 @@ CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/proxy@latest; proxy vers
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/logc@latest; logc version
 ./bind_to_443.sh `which proxy`
 sudo systemctl restart fortio
+sudo systemctl status fortio # to see the index.tsv
 tail -f proxy.log | logc -logger-force-color
