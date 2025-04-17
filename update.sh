83508d3a861bfb9dc66ab94b6ced030e5b6f4d8e
Laurent Demailly <ldemailly@gmail.com>
2025-04-17 16:42:31 -0700

    remove bad -a
    and update h2life's location

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@ set -x
 uname -a
 go version
 git pull
-CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/fortio@latest; fortio version
-CGO_ENABLED=0 go install -a -tags no_tailscale -ldflags "-s -w" fortio.org/proxy@latest; proxy version
-CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/logc@latest; logc version
-CGO_ENABLED=0 go install -a -ldflags "-s -w" github.com/fortio/terminal/life/h2life@latest; h2life version
+CGO_ENABLED=0 go install -ldflags "-s -w" fortio.org/fortio@latest; fortio version
+CGO_ENABLED=0 go install -tags no_tailscale -ldflags "-s -w" fortio.org/proxy@latest; proxy version
+CGO_ENABLED=0 go install -ldflags "-s -w" fortio.org/logc@latest; logc version
+CGO_ENABLED=0 go install -ldflags "-s -w" github.com/fortio/h2life@latest; h2life version
 ./bind_to_443.sh `which proxy`
 sudo systemctl restart fortio
 sudo systemctl status fortio # to see the index.tsv
