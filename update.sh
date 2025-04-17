01121d9f77c0dbf10ae7e7deb8fa89c8809254d5
Laurent Demailly <ldemailly@gmail.com>
2025-04-17 16:30:09 -0700

    Update update.sh
    adding h2life

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ git pull
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/fortio@latest; fortio version
 CGO_ENABLED=0 go install -a -tags no_tailscale -ldflags "-s -w" fortio.org/proxy@latest; proxy version
 CGO_ENABLED=0 go install -a -ldflags "-s -w" fortio.org/logc@latest; logc version
+CGO_ENABLED=0 go install -a -ldflags "-s -w" github.com/fortio/terminal/life/h2life@latest; h2life version
 ./bind_to_443.sh `which proxy`
 sudo systemctl restart fortio
 sudo systemctl status fortio # to see the index.tsv
