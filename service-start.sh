5530d2a2497e94d28bd21a8b8512f25fc2d33557
Laurent Demailly <ldemailly@gmail.com>
2025-04-17 16:44:31 -0700

    add h2life

--- service-start.sh
+++ service-start.sh
@@ -2,9 +2,11 @@
 set -x
 pkill fortio
 pkill proxy
+pkill h2life
 PATH=$HOME/go/bin:$PATH
 fortio server -http-port disabled -tcp-port disabled -udp-port disabled\
    -redirect-port disabled -grpc-port 8079 -P "8443 127.0.0.1:443" >> grpc.log 2>&1 &
+h2life >> h2life.log 2>&1 &
 cd fortio_data
 fortio report -http-port 8080 -redirect-port disabled >> ../report.log 2>&1 &
 cd ../fortio_https
