76e12a4b664009b08f178e3b7b0c8c5b4a835060
Laurent Demailly <ldemailly@gmail.com>
2025-04-17 22:43:14 -0700

    adding fps to installs

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ CGO_ENABLED=0 go install -ldflags "-s -w" fortio.org/fortio@latest; fortio versi
 CGO_ENABLED=0 go install -tags no_tailscale -ldflags "-s -w" fortio.org/proxy@latest; proxy version
 CGO_ENABLED=0 go install -ldflags "-s -w" fortio.org/logc@latest; logc version
 CGO_ENABLED=0 go install -ldflags "-s -w" github.com/fortio/h2life@latest; h2life version
+CGO_ENABLED=0 go install -ldflags "-s -w" github.com/fortio/fps@latest; fps version
 ./bind_to_443.sh `which proxy`
 sudo systemctl restart fortio
 sudo systemctl status fortio # to see the index.tsv
